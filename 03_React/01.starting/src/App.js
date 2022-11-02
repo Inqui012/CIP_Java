2a1def46fabfc54b23e6dce5da5df0ea5176ecb6
Inqui012 <xii0712@gmail.com>
2022-11-02 11:49:03 +0900

    binding?

--- 03_React/01.starting/src/App.js
+++ 03_React/01.starting/src/App.js
@@ -16,8 +16,8 @@ function ReactStart() {
   const styleCSS = {
     backgroundColor: 'blue',
     fontSize: '20px',
-    color: 'white'
-  }
+    color: 'white',
+  };
   return (
     // component = 코드를 재사용하기 쉽게 하기 위해서 캡슐화한 한 블록.
     // 이 안의 내용들이 component, 주석모양이 특이하네
@@ -27,13 +27,17 @@ function ReactStart() {
     // JSX에서의 연산자.
     //    A && B = A가 true 일경우 B, false 일경우 A
     //    A || B = A가 true 일경우 A, false 일경우 B
-    
+
     <>
       {/* 인라인스타일로 스타일을 주게됨 */}
-      { name === 'Var.REACT' ? <span style={styleCSS}>참</span> : <span>거짓</span>}
+      {name === 'Var.REACT' ? (
+        <span style={styleCSS}>참</span>
+      ) : (
+        <span>거짓</span>
+      )}
       <h1>Print : {name}</h1>
       {/* JSX문법에서는 Class 대신 ClassName을 사용. Class도 적용은 되나 콘솔에 오류뜸. */}
-      <h2 className='reactClass'>REACT TEST</h2>
+      <h2 className="reactClass">REACT TEST</h2>
       {def || 'TEST'}
       {def && 'TEST'}
       {/* HTML의 독립태그들도 JSX에서는 반드시 닫는태그가 있어야함. 보통은 단일태그 끝에 / 로 닫는 표시를 함. */}
@@ -45,27 +49,32 @@ function ReactStart() {
 class ClassTest extends Component {
   // 오버라이드 = 상위 Component 에 있는 함수 render()를 가져다 쓸 수 있다.
   render() {
-    const name = "REACT";
-    return <div>{name}</div>
+    const name = 'REACT';
+    return <div>{name}</div>;
   }
 }
 
 const App = () => {
-  // props = 데이터를 전달하는 방법?
+  // props = 콤포넌트 사이에서 데이터를 전달하는 방법
+  // state = 콤포넌트 안에서 데이터를 전달하는 방법
   // propsKey="value" = 인라인 속성을 주는것처럼 props 를 줄 수 있다.
   // 컴포넌트js에서 기본값 설정해줄 수도 있음
   return (
     <>
-      <MyComponent name="PropsName" forNumber={1} /><br />
+      <MyComponent name="PropsName" forNumber={1} />
+      <br />
       <MyComponent>Testing Props Children</MyComponent>
-      <MyComponent /><br />
-      <MyComponent01 /><br />
-      <ClassTest /><br />
+      <MyComponent />
+      <br />
+      <MyComponent01 />
+      <br />
+      <ClassTest />
+      <br />
       <ReactStart />
       <Counter />
       <Say />
     </>
-  )  
-}
+  );
+};
 
 export default App;
--- 03_React/02.event/src/EventPractice.jsx
+++ 03_React/02.event/src/EventPractice.jsx
@@ -1,10 +1,46 @@
 import React, { Component } from 'react'
 
 class EventPractice extends Component {
+   state = {
+      message: '',
+   }
+   constructor(props) {
+      super(props)
+      // 객체 외부에서 객체의 this를 참조할때 오류를 막기 위해 바인딩을 한다.
+      // 객체 내부에서의 this는 안정적으로 해당 객체를 지정하지만
+      // 외부 변수에 객체를 저장하게 될 때에는 이 관계가 사라져서 this 가 지정된 객체를 정확하게 찾아가지 못한다.
+
+      //    React에서는 주로 아래방법처럼 외부에 선언한 함수를 constructor 안에서 바인딩한다.
+      this.handleChange = this.handleChange.bind(this)
+      //   this.handleClick = this.handleClick.bind(this)
+   }
+   handleChange(e) {
+      this.setState({ message: e.target.value })
+   }
+   // 화살표함수를 이용하면 오토바인딩이 되어서 따로 바인딩 설정을 해주지 않아도 가능
+   handleClick = () => {
+      alert(this.state.message)
+      this.setState({
+         message: '',
+      })
+   }
    render() {
       return (
          <div>
             <h1>Practice Event</h1>
+            {/* 인라인으로 지정되는 이벤트들은 onClick, onChage 처럼 카멜방식사용 */}
+            {/* 매개변수 e 로 받는 이벤트객체 사용법은 js와 동일 */}
+            <input
+               type="text"
+               name="message"
+               placeholder="any"
+               value={this.state.message}
+               onChange={this.handleChange}
+            />
+            <button type="submit" onClick={this.handleClick}>
+               Confirm
+            </button>
+            <br />
          </div>
       )
    }
