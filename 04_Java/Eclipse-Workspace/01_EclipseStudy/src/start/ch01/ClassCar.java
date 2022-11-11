6e674239216a0d7aac924569c4ea1bbf74c3e4cd
Inqui012 <xii0712@gmail.com>
2022-11-11 17:03:59 +0900

    aaaa

--- 04_Java/Eclipse-Workspace/01_EclipseStudy/src/start/ch01/ClassCar.java
+++ 04_Java/Eclipse-Workspace/01_EclipseStudy/src/start/ch01/ClassCar.java
@@ -6,12 +6,33 @@ package start.ch01;
 public class ClassCar {
 //	field(멤버변수) = 객체의 데이터가 저장되는 장소. 객체 내부에서 또는 객체를 통해서만 사용가능.
 //	모델, 시동여부, 현재 속도를 저장할 필드.
-	String company = "hundai";
+	String company;
 	String model;
-	String color = "black";
-	int maxSpeed = 350;
+	String color;
+	int maxSpeed;
 	int speed;
 //	boolean start;
 	
+//	생성자(매개변수) {}; 로 선언한다. 매개변수를 선언할경우 외부에서 객체를 생성할때 필요한 매개변수를 입력해줘야 오류가 안남.
+//	클래스에서 따로 생성자롤 선언하지 않아도 기본적인 생성자는 자바에서 자동으로 생성해줌.
+//	객체가 생성되는 순간 실행된다. 생성자는 클래스명과 같은 이름을 가져야한다.
+	ClassCar(String model, String color, int speed) {
+//		필드 초기화. 객체 안에서 데이터를 지칭할때 this. 사용 가능
+		this.model = model;
+		this.color = color;
+		this.speed = speed;
+	}
+	
+//	생성자 overload. 여러개의 생성자를 설정해놓을 수 있다. 보통 들어오는 매개변수의 갯수/타입에 맞춘 생성자가 실행되는것 같음.
+//	생성할때 매개변수의 타입이나 순서 둘중 하나는 다르게 들어가있어야 한다.
+//	생성자 안에서 다른 생성자를 실행시키는것도 가능한것 같은데
+	ClassCar(String model) {
+		this.model = model;
+//		아래경우 받아온 매개변수 model을 입력하고 나머지 값을 상위에 있는 다른 생성자로 사용해서 생성.
+//		this(model, "red", 204)
+	}
+	ClassCar(int maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
 
 }
--- 04_Java/Eclipse-Workspace/01_EclipseStudy/src/start/ch01/ClassTest.java
+++ 04_Java/Eclipse-Workspace/01_EclipseStudy/src/start/ch01/ClassTest.java
@@ -5,7 +5,7 @@ public class ClassTest {
 	public static void main(String[] args) {
 //		메인에서 객체를 사용하기 위해서는 우선 생성부터
 //		아래는 ClassCar 클래스의 car01 인스턴스. 클래스로 실제 생성된 객체를 인스턴스라고 한다.
-		ClassCar car01 = new ClassCar();
+		ClassCar car01 = new ClassCar("K9", "Yellow", 450);
 		
 //		변수자체는 객체를 생성한 파일이 가지고있는 메모리를 참조하는듯?
 //		실제 객체는 heap 영역에 저장되고 변수에는 해당 객체로 갈 수 있는 메모리를 참조. 참조타입.
@@ -13,9 +13,15 @@ public class ClassTest {
 		System.out.println(car01);
 		System.out.println("car01 : " + car01.company + " / " + car01.model + " / " + car01.color + " / " + car01.speed + " / " + car01.maxSpeed);
 
-		ClassCar car02 = new ClassCar();
+		ClassCar car02 = new ClassCar("Roll", "Blue", 230);
 		car02.color = "green";
 		System.out.println("car02 : " + car02.company + " / " + car02.model + " / " + car02.color + " / " + car02.speed + " / " + car02.maxSpeed);
+
+		ClassCar car03 = new ClassCar("Roll");
+		System.out.println("car03 : " + car03.company + " / " + car03.model + " / " + car03.color + " / " + car03.speed + " / " + car03.maxSpeed);
+
+		ClassCar car04 = new ClassCar(999);
+		System.out.println("car04 : " + car04.company + " / " + car04.model + " / " + car04.color + " / " + car04.speed + " / " + car04.maxSpeed);
 	}
 
 }
